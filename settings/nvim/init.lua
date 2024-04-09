f25e187a21e4cbcc5967ba22679e6589d237cd70
Trevor Atlas <me@trevoratlas.com>
2024-04-09 17:10:54 -0400

    fix some stuff, add better rust support (I HOPE)

--- settings/nvim/init.lua
+++ settings/nvim/init.lua
@@ -1,144 +1,168 @@
-require('options')
+require("options")
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  'nvim-lua/plenary.nvim',
-  'nvim-tree/nvim-web-devicons',
-  'ggandor/lightspeed.nvim',
-  'simrat39/rust-tools.nvim',
-  { 'folke/neoconf.nvim', config = function() end },
+require("lazy").setup({
+  "nvim-lua/plenary.nvim",
+  "nvim-tree/nvim-web-devicons",
+  "ggandor/lightspeed.nvim",
+  -- "simrat39/rust-tools.nvim",
+  { "folke/neoconf.nvim", config = function() end },
   {
-    'brenoprata10/nvim-highlight-colors',
-    init = function() require('nvim-highlight-colors').setup({}) end,
+    "brenoprata10/nvim-highlight-colors",
+    init = function()
+      require("nvim-highlight-colors").setup({})
+    end,
   },
   {
-    'numToStr/Comment.nvim',
+    "numToStr/Comment.nvim",
     opts = {},
     lazy = false,
   },
   {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   },
   {
-    'numToStr/Navigator.nvim',
-    config = function() require('Navigator').setup({}) end,
+    "numToStr/Navigator.nvim",
+    config = function()
+      require("Navigator").setup({})
+    end,
   },
   {
-    'folke/trouble.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   },
-  'MunifTanjim/nui.nvim',
+  "MunifTanjim/nui.nvim",
   {
     -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim', -- Useful status updates for LSP
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim", -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} }, -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
+      { "j-hui/fidget.nvim", tag = "legacy", opts = {} }, -- Additional lua configuration, makes nvim stuff amazing!
+      "folke/neodev.nvim",
     },
-    config = function() require('lspconfig').eslint.setup({}) end,
+    config = function()
+      require("lspconfig").eslint.setup({})
+    end,
   },
   {
 
-    'L3MON4D3/LuaSnip',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
     opts = {
       history = true,
-      delete_check_events = 'TextChanged',
-      region_check_events = 'CursorMoved',
+      delete_check_events = "TextChanged",
+      region_check_events = "CursorMoved",
     },
   },
   {
     -- Autocompletion
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
 
       -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
+      "hrsh7th/cmp-nvim-lsp",
 
       -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+      "rafamadriz/friendly-snippets",
     },
   },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
+    "lewis6991/gitsigns.nvim",
     opts = {
       -- See `:help gitsigns.txt`
       --
       signs = {
-        add = { hl = 'GitGutterAdd', text = '+' },
-        change = { hl = 'GitGutterChange', text = '~' },
-        delete = { hl = 'GitGutterDelete', text = '-' },
-        topdelete = { hl = 'GitGutterDelete', text = '‾' },
-        changedelete = { hl = 'GitGutterChange', text = '~' },
+        add = { hl = "GitGutterAdd", text = "+" },
+        change = { hl = "GitGutterChange", text = "~" },
+        delete = { hl = "GitGutterDelete", text = "-" },
+        topdelete = { hl = "GitGutterDelete", text = "‾" },
+        changedelete = { hl = "GitGutterChange", text = "~" },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
-        vim.keymap.set('n', '<leader>lb', require('gitsigns').toggle_current_line_blame, { buffer = bufnr, desc = 'Preview git hunk' })
+        vim.keymap.set(
+          "n",
+          "<leader>hp",
+          require("gitsigns").preview_hunk,
+          { buffer = bufnr, desc = "Preview git hunk" }
+        )
+        vim.keymap.set(
+          "n",
+          "<leader>lb",
+          require("gitsigns").toggle_current_line_blame,
+          { buffer = bufnr, desc = "Preview git hunk" }
+        )
 
         -- don't override the built-in and fugitive keymaps
         local gs = package.loaded.gitsigns
-        vim.keymap.set({ 'n', 'v' }, ']c', function()
-          if vim.wo.diff then return ']c' end
+        vim.keymap.set({ "n", "v" }, "]c", function()
+          if vim.wo.diff then
+            return "]c"
+          end
           vim.schedule(gs.next_hunk)
-          return '<Ignore>'
-        end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-        vim.keymap.set({ 'n', 'v' }, '[c', function()
-          if vim.wo.diff then return '[c' end
+          return "<Ignore>"
+        end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+        vim.keymap.set({ "n", "v" }, "[c", function()
+          if vim.wo.diff then
+            return "[c"
+          end
           vim.schedule(gs.prev_hunk)
-          return '<Ignore>'
-        end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+          return "<Ignore>"
+        end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
       end,
     },
   },
   {
-    'mhartington/formatter.nvim',
-    config = function() require('fmt') end,
+    "mhartington/formatter.nvim",
+    config = function()
+      require("fmt")
+    end,
   },
   {
-    'folke/tokyonight.nvim',
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
-    init = function() vim.cmd.colorscheme('tokyonight-storm') end,
+    init = function()
+      vim.cmd.colorscheme("tokyonight-storm")
+    end,
   },
   {
     -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     -- See `:help lualine.txt`
     init = function()
       local colors = {
-        blue = '#80a0ff',
-        cyan = '#79dac8',
-        black = '#080808',
-        white = '#c6c6c6',
-        red = '#ff5189',
-        violet = '#d183e8',
-        grey = '#303030',
+        blue = "#80a0ff",
+        cyan = "#79dac8",
+        black = "#080808",
+        white = "#c6c6c6",
+        red = "#ff5189",
+        violet = "#d183e8",
+        grey = "#303030",
       }
       local theme = {
         normal = {
@@ -157,29 +181,29 @@ require('lazy').setup({
           c = { fg = colors.black, bg = colors.black },
         },
       }
-      require('lualine').setup({
+      require("lualine").setup({
         theme,
-        component_separators = '|',
-        section_separators = { left = '', right = '' },
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
         sections = {
           lualine_a = {
-            { 'mode', separator = { left = '' }, right_padding = 2 },
+            { "mode", separator = { left = "" }, right_padding = 2 },
           },
-          lualine_b = { 'filename', 'branch', 'diff' },
-          lualine_c = { 'diagnostics', 'encoding' },
+          lualine_b = { "filename", "branch", "diff" },
+          lualine_c = { "diagnostics", "encoding" },
           lualine_x = {},
-          lualine_y = { 'filetype', 'searchcount' },
+          lualine_y = { "filetype", "searchcount" },
           lualine_z = {
-            { 'location', separator = { right = '' }, left_padding = 2 },
+            { "location", separator = { right = "" }, left_padding = 2 },
           },
         },
         inactive_sections = {
-          lualine_a = { 'filename' },
+          lualine_a = { "filename" },
           lualine_b = {},
           lualine_c = {},
           lualine_x = {},
           lualine_y = {},
-          lualine_z = { 'location' },
+          lualine_z = { "location" },
         },
         tabline = {},
         extensions = {},
@@ -188,174 +212,182 @@ require('lazy').setup({
   },
   {
     -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    build = ':TSUpdate',
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+    build = ":TSUpdate",
   },
-  'echasnovski/mini.bufremove',
-  { 'max397574/better-escape.nvim', event = 'InsertCharPre', opts = { timeout = 300 } },
+  "echasnovski/mini.bufremove",
+  { "max397574/better-escape.nvim", event = "InsertCharPre", opts = { timeout = 300 } },
   {
-    'NMAC427/guess-indent.nvim',
+    "NMAC427/guess-indent.nvim",
     config = function(_, opts)
-      require('guess-indent').setup(opts)
+      require("guess-indent").setup(opts)
       vim.cmd.lua({ args = { "require('guess-indent').set_from_buffer('auto_cmd')" }, mods = { silent = true } })
     end,
   },
   {
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    opts = { picker_config = { statusline_winbar_picker = { use_winbar = 'smart' } } },
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    opts = { picker_config = { statusline_winbar_picker = { use_winbar = "smart" } } },
   },
   {
-    'folke/which-key.nvim',
-    event = 'VeryLazy',
+    "folke/which-key.nvim",
+    event = "VeryLazy",
     init = function()
-      local wk = require('which-key')
-      wk.register({
-        p = {
-          name = "ChatGPT",
-            c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-            e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-            g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-            t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-            k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-            d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-            a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-            o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-            s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-            f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-            x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-            r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-            l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
-          },
-      })
+      -- local wk = require('which-key')
+      -- wk.register({
+      --   p = {
+      --     name = "ChatGPT",
+      --       c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+      --       e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+      --       g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+      --       t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+      --       k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+      --       d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+      --       a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+      --       o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+      --       s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+      --       f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+      --       x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+      --       r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+      --       l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+      --     },
+      -- })
       vim.o.timeout = true
       vim.o.timeoutlen = 300
     end,
     opts = {
-      icons = { group = vim.g.icons_enabled and '' or '+', separator = '' },
-      disable = { filetypes = { 'TelescopePrompt' } },
+      icons = { group = vim.g.icons_enabled and "" or "+", separator = "" },
+      disable = { filetypes = { "TelescopePrompt" } },
     },
   },
   {
-    'akinsho/toggleterm.nvim',
-    cmd = { 'ToggleTerm', 'TermExec' },
+    "akinsho/toggleterm.nvim",
+    cmd = { "ToggleTerm", "TermExec" },
     init = function() end,
     opts = {
       highlights = {
-        Normal = { link = 'Normal' },
-        NormalNC = { link = 'NormalNC' },
-        NormalFloat = { link = 'NormalFloat' },
-        FloatBorder = { link = 'FloatBorder' },
-        StatusLine = { link = 'StatusLine' },
-        StatusLineNC = { link = 'StatusLineNC' },
-        WinBar = { link = 'WinBar' },
-        WinBarNC = { link = 'WinBarNC' },
+        Normal = { link = "Normal" },
+        NormalNC = { link = "NormalNC" },
+        NormalFloat = { link = "NormalFloat" },
+        FloatBorder = { link = "FloatBorder" },
+        StatusLine = { link = "StatusLine" },
+        StatusLineNC = { link = "StatusLineNC" },
+        WinBar = { link = "WinBar" },
+        WinBarNC = { link = "WinBarNC" },
       },
       size = 10,
       on_create = function(term)
-        vim.opt.foldcolumn = '0'
-        vim.opt.signcolumn = 'no'
-        vim.cmd('startinsert!')
+        vim.opt.foldcolumn = "0"
+        vim.opt.signcolumn = "no"
+        vim.cmd("startinsert!")
       end,
       open_mapping = [[<c-\>]],
       shading_factor = 2,
-      direction = 'float',
+      direction = "float",
       float_opts = {
-        border = 'curved',
+        border = "curved",
         highlights = {
-          border = 'Normal',
-          background = 'Normal',
+          border = "Normal",
+          background = "Normal",
         },
       },
     },
   },
   {
-    'nvim-tree/nvim-web-devicons',
+    "nvim-tree/nvim-web-devicons",
     enabled = vim.g.icons_enabled,
     opts = {
       override = {
-        default_icon = { icon = '[F]' },
-        deb = { icon = '', name = 'Deb' },
-        lock = { icon = '󰌾', name = 'Lock' },
-        mp3 = { icon = '󰎆', name = 'Mp3' },
-        mp4 = { icon = '', name = 'Mp4' },
-        out = { icon = '', name = 'Out' },
-        ['robots.txt'] = { icon = '󰚩', name = 'Robots' },
-        ttf = { icon = '', name = 'TrueTypeFont' },
-        rpm = { icon = '', name = 'Rpm' },
-        woff = { icon = '', name = 'WebOpenFontFormat' },
-        woff2 = { icon = '', name = 'WebOpenFontFormat2' },
-        xz = { icon = '', name = 'Xz' },
-        zip = { icon = '', name = 'Zip' },
+        default_icon = { icon = "[F]" },
+        deb = { icon = "", name = "Deb" },
+        lock = { icon = "󰌾", name = "Lock" },
+        mp3 = { icon = "󰎆", name = "Mp3" },
+        mp4 = { icon = "", name = "Mp4" },
+        out = { icon = "", name = "Out" },
+        ["robots.txt"] = { icon = "󰚩", name = "Robots" },
+        ttf = { icon = "", name = "TrueTypeFont" },
+        rpm = { icon = "", name = "Rpm" },
+        woff = { icon = "", name = "WebOpenFontFormat" },
+        woff2 = { icon = "", name = "WebOpenFontFormat2" },
+        xz = { icon = "", name = "Xz" },
+        zip = { icon = "", name = "Zip" },
       },
     },
   },
   {
-    'onsails/lspkind.nvim',
-    config = function(_, opts) require('lspkind').init(opts) end,
+    "onsails/lspkind.nvim",
+    config = function(_, opts)
+      require("lspkind").init(opts)
+    end,
   },
   {
-    'rcarriga/nvim-notify',
+    "rcarriga/nvim-notify",
     opts = {
       on_open = function(win)
         vim.api.nvim_win_set_config(win, { zindex = 175 })
-        if not package.loaded['nvim-treesitter'] then pcall(require, 'nvim-treesitter') end
+        if not package.loaded["nvim-treesitter"] then
+          pcall(require, "nvim-treesitter")
+        end
         vim.wo[win].conceallevel = 3
         local buf = vim.api.nvim_win_get_buf(win)
-        if not pcall(vim.treesitter.start, buf, 'markdown') then vim.bo[buf].syntax = 'markdown' end
+        if not pcall(vim.treesitter.start, buf, "markdown") then
+          vim.bo[buf].syntax = "markdown"
+        end
         vim.wo[win].spell = false
       end,
     },
   },
   {
-    'stevearc/dressing.nvim',
+    "stevearc/dressing.nvim",
     opts = {
-      input = { default_prompt = '➤ ' },
-      select = { backend = { 'telescope', 'builtin' } },
+      input = { default_prompt = "➤ " },
+      select = { backend = { "telescope", "builtin" } },
     },
   },
   {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     opts = {
-      indent = { char = '▏' },
+      indent = { char = "▏" },
       scope = { show_start = false, show_end = false },
       exclude = {
         buftypes = {
-          'nofile',
-          'terminal',
+          "nofile",
+          "terminal",
         },
         filetypes = {
-          'help',
-          'startify',
-          'aerial',
-          'alpha',
-          'dashboard',
-          'lazy',
-          'neogitstatus',
-          'NvimTree',
-          'neo-tree',
-          'Trouble',
+          "help",
+          "startify",
+          "aerial",
+          "alpha",
+          "dashboard",
+          "lazy",
+          "neogitstatus",
+          "NvimTree",
+          "neo-tree",
+          "Trouble",
         },
       },
     },
   },
   {
-    'nvim-telescope/telescope-frecency.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
-    config = function() require('telescope').load_extension('frecency') end,
+    "nvim-telescope/telescope-frecency.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("telescope").load_extension("frecency")
+    end,
   },
-  { import = 'custom.plugins' },
+  { import = "custom.plugins" },
   -- setup args explained: https://github.com/folke/lazy.nvim?tab=readme-ov-file#-plugin-spec
 }, {})
 
-require('globals')
-require('hubspot-bender').setup()
-require('treesitter')
-require('lsp')
-require('rust')
-require('completion')
-require('autocommands')
-require('theme').setup()
-require('mappings')
+require("globals")
+require("hubspot-bender").setup()
+require("treesitter")
+require("lsp")
+-- require("rust")
+require("completion")
+require("autocommands")
+require("theme").setup()
+require("mappings")
--- settings/nvim/lua/lsp.lua
+++ settings/nvim/lua/lsp.lua
@@ -1,40 +1,46 @@
 local float = {
   focusable = true,
-  style = 'minimal',
-  border = 'rounded',
+  style = "minimal",
+  border = "rounded",
 }
 
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, float)
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, float)
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, float)
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float)
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc)
-    if desc then desc = 'LSP: ' .. desc end
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    if desc then
+      desc = "LSP: " .. desc
+    end
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader><S-d>', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  vim.keymap.set('n', 'gh', function() vim.diagnostic.open_float({ bufnr = 0 }) end, { remap = true, silent = true })
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+  nmap("<leader><S-d>", vim.lsp.buf.type_definition, "Type [D]efinition")
+  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+  vim.keymap.set("n", "gh", function()
+    vim.diagnostic.open_float({ bufnr = 0 })
+  end, { remap = true, silent = true })
 
   -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
   --nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
   --nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   --nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   --nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
+  nmap("<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, "[W]orkspace [L]ist Folders")
 
   -- Create a command `:Format` local to the LSP buffer
   --vim.api.nvim_buf_create_user_command(bufnr, 'Format', vim.lsp.buf.format, { desc = 'Format current buffer with LSP' })
@@ -52,31 +58,30 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  prettier = {},
-  rust_analyzer = {
-    checkOnSave = {
-      command = 'clippy',
-    },
-  },
-  html = { filetypes = { 'html' } },
-  yamlls = { filetypes = { 'lyaml', 'yaml' } },
+  -- rust_analyzer = {
+  --   checkOnSave = {
+  --     command = "clippy",
+  --   },
+  -- },
+  html = { filetypes = { "html" } },
+  yamlls = { filetypes = { "lyaml", "yaml" } },
   lua_ls = { Lua = { workspace = { checkThirdParty = false }, telemetry = { enable = false } } },
 }
 
 -- Setup neovim lua configuration
-require('neodev').setup()
+require("neodev").setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Ensure the servers above are installed
-local mason_lspconfig = require('mason-lspconfig')
+local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup({ ensure_installed = vim.tbl_keys(servers) })
 mason_lspconfig.setup_handlers({
   function(server_name)
-    require('lspconfig')[server_name].setup({
+    require("lspconfig")[server_name].setup({
       capabilities = capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
@@ -85,4 +90,4 @@ mason_lspconfig.setup_handlers({
   end,
 })
 
-require('typescript')
+require("typescript")
