0ba130c45d033ea6ce46942da9c812a2e14c1c1b
Trevor Atlas <me@trevoratlas.com>
2024-08-26 09:34:38 -0400

    neovim stuff

--- settings/nvim/lua/custom/plugins/chat.lua
+++ settings/nvim/lua/custom/plugins/chat.lua
@@ -1,13 +1,52 @@
+-- return {
+--   "olimorris/codecompanion.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-treesitter/nvim-treesitter",
+--     "nvim-telescope/telescope.nvim",
+--   },
+--   config = function()
+--     require("codecompanion").setup({
+--       strategies = {
+--         chat = {
+--           adapter = "anthropic",
+--         },
+--         inline = {
+--           adapter = "anthropic",
+--         },
+--         agent = {
+--           adapter = "anthropic",
+--         },
+--       },
+--       adapters = {
+--         anthropic = function()
+--           return require("codecompanion.adapters").extend("anthropic", {
+--             env = {
+--               api_key = "ANTHROPIC_API_KEY",
+--             },
+--           })
+--         end,
+--       },
+--     })
+--   end,
+-- }
 return {
-  "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim"
+  'robitx/gp.nvim',
+  config = function()
+    local conf = {
+      providers = {
+        openai = {
+          disable = true,
+        },
+        anthropic = {
+          disable = false,
+          -- endpoint = 'https://api.anthropic.com/v1/messages',
+          -- secret = os.getenv('ANTHROPIC_API_KEY'),
+        },
+      },
     }
+    require('gp').setup(conf)
+
+    -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+  end,
 }
--- settings/nvim/lua/fmt.lua
+++ settings/nvim/lua/fmt.lua
@@ -7,32 +7,31 @@ local function prettier_config()
   local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
   local config_path = config_pattern(bufname)
 
-  local exe = utils.is_hubspot_machine and 'bpx'
-    or config_path .. '/node_modules/prettier/node_modules/.bin/prettier'
+  local exe = utils.is_hubspot_machine and 'bpx' or config_path .. '/node_modules/prettier/node_modules/.bin/prettier'
 
   return {
     exe = exe,
-    args = {'hs-prettier', '--stdin-filepath', vim.api.nvim_buf_get_name(0), '--config', config_path .. '/prettier.config.js' },
+    args = { 'hs-prettier', '--stdin-filepath', vim.api.nvim_buf_get_name(0), '--config', config_path .. '/prettier.config.js' },
     stdin = true,
   }
 end
 
 local function lsp_or_leptos()
-  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  -- local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
   -- true if any line contains "leptos"
-  local probably_leptos_project = vim.iter(lines):any(function(content) return content:match('leptos') end)
+  -- local probably_leptos_project = vim.iter(lines):any(function(content) return content:match('leptos') end)
 
-  if probably_leptos_project == true then
-    return {
-      exe = 'leptosfmt',
-      args = { vim.api.nvim_buf_get_name(0), '--stdin', '--rustfmt', '--quiet' },
-      stdin = true,
-    }
-  end
+  -- if probably_leptos_project == true then
+  --   return {
+  --     exe = 'leptosfmt',
+  --     args = { vim.api.nvim_buf_get_name(0), '--stdin', '--rustfmt', '--quiet' },
+  --     stdin = true,
+  --   }
+  -- end
   return {
     exe = 'rustfmt',
-    args = { vim.api.nvim_buf_get_name(0) },
+    args = { vim.api.nvim_buf_get_name(0), '--stdin' },
     stdin = true,
   }
 end
