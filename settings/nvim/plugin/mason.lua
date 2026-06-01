27c8e21d748b127170598bc2d1e6925273a4574f
trevor-atlas <me@trevoratlas.com>
2026-06-01 13:44:26 -0400

    defer copilot and mason until they are needed
    Why:
    - Copilot and Mason were still two of the biggest startup costs even after the dead-plugin cleanup
    - both features can be initialized on first use without changing their everyday workflows
    
    What changed:
    - moved Copilot setup behind InsertEnter and a lightweight :Copilot stub command
    - moved Mason setup behind VimEnter scheduling and a lightweight :Mason stub command
    - kept the existing keymap and command entrypoints working while reducing startup work

--- settings/nvim/plugin/mason.lua
+++ settings/nvim/plugin/mason.lua
@@ -1,14 +1,36 @@
-vim.pack.add({
-  'https://github.com/mason-org/mason.nvim',
-  'https://github.com/mason-org/mason-lspconfig.nvim'
-})
+local mason_loaded = false
 
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    'eslint',
-    'lua_ls',
-    'ts_ls',
-    'yamlls',
-  },
+local function setup_mason()
+  if mason_loaded then return end
+
+  pcall(vim.api.nvim_del_user_command, 'Mason')
+
+  vim.pack.add({
+    'https://github.com/mason-org/mason.nvim',
+    'https://github.com/mason-org/mason-lspconfig.nvim',
+  })
+
+  require('mason').setup()
+  require('mason-lspconfig').setup({
+    ensure_installed = {
+      'eslint',
+      'lua_ls',
+      'ts_ls',
+      'yamlls',
+    },
+  })
+
+  mason_loaded = true
+end
+
+vim.api.nvim_create_user_command('Mason', function(opts)
+  setup_mason()
+  vim.cmd('Mason ' .. opts.args)
+end, { nargs = '*' })
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  once = true,
+  callback = function()
+    vim.schedule(setup_mason)
+  end,
 })
